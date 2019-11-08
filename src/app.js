61d0d59c932845f0c8778304165d1918c10fcc58
srinivasa <srinivasa.venkataiah@publicissapient.com>
2019-11-08 20:48:00 +0530

    set up app for heroku

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ console.log(__dirname)
 console.log(path.join(__dirname , '../public'))
 
 const app = express()
-
+const port = process.env.PORT || 3000
 
 //define paths for express config
 const publicDirectory = path.join(__dirname , '../public')
@@ -111,6 +111,6 @@ app.get('*' , (req, res) => {
     })
 })
 
-app.listen(3000 , () => {
-    console.log('server is up on port 3000')
+app.listen(port , () => {
+    console.log('server is up on port' + port)
 })
